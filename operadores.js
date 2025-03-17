17486e8d1e6f131033ab4f8bcfc68eb2548cecb0
Engel <angitsdev@pm.me>
2025-03-17 00:04:40 -0600

    🔫

--- operadores.js
+++ operadores.js
@@ -1,26 +1,22 @@
 // Los operadores nos van a permitir realizar operaciones al utilizar valores y variables
 // Tenemos distintos tipos de operadores
 
+// Operadores aritmeticos
 // (+) suma
 let suma = 5 + 9;
-
 // (-) resta
 let resta = 9 - 8;
-
 // (*) multiplicación
 let multiplicacion = 5 * 2;
-
 // (/) división
 let division = 10 / 2;
-
 // (%) módulo o residuo de la división
 let modulo = 9 / 2; // 1
-
 // (**) potencia o exponente
 let potencia = 2 ** 3;  // Este operador se encuentra a partir de la versión de ECMAScript 2016
 // Si la versión es inferior tendremos que usar Math.pow(numero, exponente)
 
-let a, b, c, d
+let a, b, c, d;
 a = 8;
 b = 6;
 
@@ -34,10 +30,10 @@ let predecremento = --b;    // Decrementa en 1 el valor de la variable en esta m
 // Y posteriormente la proxima vez que usemos esta variable, se realizará el incremento o decremento según el caso
 
 // (variable++) post-incremento
-c = a++ * b // La variable 'a' en esta línea sigue con el valor de 9, cuando se vuelva a usar se incrementará su valor
+c = a++ * b; // La variable 'a' en esta línea sigue con el valor de 9, cuando se vuelva a usar se incrementará su valor
 
 // (variable--) post-decremento
-d = a / b-- // La variable b en esta línea conserva el valor de 5 y la variable a tendrá el valor de 10 por su incremento pendiente
+d = a / b--; // La variable b en esta línea conserva el valor de 5 y la variable a tendrá el valor de 10 por su incremento pendiente
 
 // Para asignar valores a nuestras variables usamos el operador de asignación (=)
 let age = 19;
@@ -48,24 +44,24 @@ age -= 1;   // Esto equivale a age = age - 1 y así con los demás operadores de
 // Otros operadores compuestos +=, -=, *=, /=, %=, **=
 
 // Operadores de comparación o relacionales
-// == operador de igualdad (valor) si es necesario realiza una conversión del tipo de dato para poder comparar los valores
+// (==) operador de igualdad (valor) si es necesario realiza una conversión del tipo de dato para poder comparar los valores
 let igualdad = 9 == '9';    // true (ya que el string '9' se convierte a number)
-// === operador de igualdad exacto o estricto (tipo y valor)
+// (===) operador de igualdad exacto o estricto (tipo y valor)
 let igualdadExacta = 9 === '9'; // false (ya que un number no es igual a un string)
 
-// != operador de distinto a (valor) (si es necesario realiza una conversión de tipo de dato para poder comparar)
+// (!=) operador de distinto a (valor) (si es necesario realiza una conversión de tipo de dato para poder comparar)
 let distinto = 9 != '9'; // false (ya que el string '9' se convierte a number y esto los hace iguales)
-// !+== operador de distinto exacto a
+// (!+==) operador de distinto exacto a
 let distintoExacto = 9 !== '9'; // true (ya que un number es desigual a un string)
 
 // En estos operadores también se realiza una conversión de tipo de dato si es necesario para poder comparar
-// > operador mayor que
+// (>) operador mayor que
 let mayor = 9 > 8;  // true (ya que 9 es mayor que 8)
-// >= operador mayor o igual que
+// (>=) operador mayor o igual que
 let mayorIgual = '9' > 9;   // true ya que 9 en string se convierte a entero y no es mayor que 9 pero es igual)
-// < operador menor que
+// (<) operador menor que
 let menor = 8 < 6;  //  false (ya que 8 no es menor que 6) 
-// <= operador menor o igual que
+// (<=) operador menor o igual que
 let menorIgual = 7 <= 9;    // true (ya que 7 no es igual que 9 pero es menor)
 
 // String interpolation significa que dentro de una misma cadena podemos agregar expresiones
@@ -76,16 +72,16 @@ console.log(`Hola ${nombre}, cómo estás?`);  //  La variable o expresión se c
 console.lof(`Es 9 mayor a 10? -> ${9 > 10}`);   // Es 9 mayor a 10? -> false
 
 // Operadores lógicos
-// && operador and (y), regresa true si ambos operandos son verdaderos, en todo caso contrario regresa false
+// (&&) operador and (y), regresa true si ambos operandos son verdaderos, en todo caso contrario regresa false
 console.log(false && true);  // false (operador de cortocircuito, ya que si la expresión del lado izquierdo es falsa, ni siquiera se evalúa la otra expresión)
 console.log(true && false); // En este caso todavía vale la pena evaluar la expresión del lado derecho (igualmente da false)
 
-// || operador or (ó), a este símbolo (||) se le llama pipe, regresa true si uno de los operandos es verdadero
+// (||) operador or (ó), a este símbolo (||) se le llama pipe, regresa true si uno de los operandos es verdadero
 // Solamente en caso de que ambas expresiones sean falsas regresa false
 console.log(false || true); // true (si la expresión de la izquierda es falsa, aún hay posibilidades de que la expresión de la derecha sea verdadera por lo tanto sí se evalúa)
 console.log(false || false);    // false
 
-//  ! operador de negación not (no), invierte el resultado logico de nuestra expresión
+// (!) operador de negación not (no), invierte el resultado logico de nuestra expresión
 // Es un operador unario (requiere de un valor) a diferencia de && y || que son binarios (requieren de dos valores)
 console.log(!true); // false
 console.log(!false);    // true
