2959f96463aa9c33b950d5ae9cda25b26346d84d
Engel <angitsdev@pm.me>
2025-03-26 21:43:34 -0600

    🔫

--- ciclos.js
+++ ciclos.js
@@ -3,7 +3,7 @@
 // Ciclo while
 // El ciclo while nos va a permitir repetir un bloque de codigo tantas veces como la condición sea true
 // Cuando la condición sea false, se dejará de ejecutar el ciclo while
-// While significa mientras, si desde el inicio la condición del while es false, no se ejecuta el ciclo while
+// While significa mientras, si desde el inicio la condición del while es false no se ejecutará el ciclo while
 // Su sintaxis es la palabra reservada while seguido de parentesis y en ellos la condición
 // Y seguido de llaves para definir el bloque de codigo del ciclo
 let contador = 0, repeticiones = 5;
@@ -11,8 +11,7 @@ while (contador <= repeticiones) {
     console.log(contador++);    // Sin esta expresión (contador++) sería un ciclo infinito
     // Una iteración es la repetición de nuestro bloque de codigo
 }
-// Si el bloque de codigo del ciclo while solamente incluye una linea
-// Se pueden omitir las llaves
+// Si el bloque de codigo del ciclo while solamente incluye una linea se pueden omitir las llaves
 
 // Ciclo do while
 // El ciclo do while ejecuta al menos una vez su bloque de codigo y posteriormente se evalúa la condicion
